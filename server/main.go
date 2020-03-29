c97042f8e458ab00725b1b07eb6b741e5fe9e3a9
Will Refvem <wbrefvem@gmail.com>
2020-03-29 03:24:50 -0400

    Initial implementation of server logic

--- server/main.go
+++ server/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 // Crawlable represents a crawable unit, i.e. domain
@@ -14,43 +17,74 @@ type Crawlable struct {
 	URL string
 }
 
+func processURL(rawURL string) (*url.URL, error) {
+	log.Printf("process URL %s\n", rawURL)
+	parsed, err := url.Parse(rawURL)
+	if err != nil {
+		return nil, err
+	}
+
+	if parsed.Scheme != "http" && parsed.Scheme != "https" {
+		return nil, fmt.Errorf("URL scheme is invalid")
+	}
+
+	return parsed, nil
+}
+
 func handleGet(w http.ResponseWriter, r *http.Request) {
 	log.Println("Processing GET...")
 }
 
 func handlePost(w http.ResponseWriter, r *http.Request) {
+	log.Println("Processing POST")
 	var c Crawlable
 
 	err := json.NewDecoder(r.Body).Decode(&c)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Printf("ERROR: %s\n", err)
+		http.Error(w, "JSON body is malformed", http.StatusBadRequest)
 		return
 	}
 
-	log.Printf("Crawling domain %s\n", c.URL)
+	parsedURL, err := processURL(c.URL)
+	if err != nil {
+		log.Printf("ERROR: %s\n", err)
+		http.Error(w, "Invalid URL", http.StatusBadRequest)
+		return
+	}
+
+	domainAndPort := strings.Split(parsedURL.Host, ":")
+	bareDomain := domainAndPort[0]
+
+	log.Printf("Crawling domain %s\n", bareDomain)
 
 	cmd := exec.Command(
 		"scrapy",
 		"crawl",
 		"osc",
 		"-a",
-		fmt.Sprintf("allowed_domains=%s", c.URL),
+		fmt.Sprintf("allowed_domains=%s,", bareDomain),
 		"-a",
-		fmt.Sprintf("start_urls=%s", c.URL),
+		fmt.Sprintf("start_urls=%s,", fmt.Sprintf("%s://%s/%s", parsedURL.Scheme, parsedURL.Host, parsedURL.Path)),
 	)
 
-	cmd.Dir = "/Users/wrefvem/go/src/github.com/wbrefvem/osc-gateway"
+	cmd.Dir = os.Getenv("WORK_DIR")
+	if cmd.Dir == "" {
+		cmd.Dir = "/opt/crawler"
+	}
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	err = cmd.Run()
+	err = cmd.Start()
 	if err != nil {
-		log.Printf("ERROR: %s", err)
+		log.Printf("ERROR: %s\n", err)
+		http.Error(w, "failed to start crawl command", http.StatusInternalServerError)
 	}
+
+	io.WriteString(w, fmt.Sprintf("started crawl for domain %s", c.URL))
 }
 
 func handleRequests(w http.ResponseWriter, r *http.Request) {
-	log.Println("handling request...")
 	if r.Method == http.MethodGet {
 		handleGet(w, r)
 	} else if r.Method == http.MethodPost {
