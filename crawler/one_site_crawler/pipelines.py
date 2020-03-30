141e14380f5246581062378d8f1f72dbae607f57
Will Refvem <wbrefvem@gmail.com>
2020-03-29 22:38:38 -0400

    Implement custom json processing in crawler

--- crawler/one_site_crawler/pipelines.py
+++ crawler/one_site_crawler/pipelines.py
@@ -4,8 +4,71 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import os
+import json
 
+from urllib.parse import urlparse
+from os import path
+from datetime import datetime
 
 class OneSiteCrawlerPipeline(object):
+
     def process_item(self, item, spider):
+        self.outfile.seek(0)
+        try:
+            sitemap = json.load(self.outfile)
+        except json.JSONDecodeError:
+            self.outfile.close()
+            spider.crawler.stop()
+        sitemap['data'].append(item)
+
+        self.outfile.seek(0)
+        self.outfile.truncate()
+        try:
+            json.dump(sitemap, self.outfile)
+        except json.JSONDecodeError:
+            self.outfile.close()
+            spider.crawler.stop()
         return item
+
+    def open_spider(self, spider):
+        data_dir = os.getenv("DATA_DIR", "/opt/data")
+        domain = spider.allowed_domains[0]
+        file_path = path.join(data_dir, "%s.json" % domain)
+
+        sitemap = {
+            'last-crawled': 'never',
+            'domain': domain,
+            'crawl-status': 'pending',
+            'data': [],
+        }
+
+        self.outfile = open(file_path, 'a+')
+        self.outfile.seek(0)
+            
+        # If we just created the file, the JSON decode will fail
+        try:
+            data = json.load(self.outfile)
+            self.outfile.seek(0)
+        except json.JSONDecodeError:
+            data = sitemap
+        
+        self.outfile.truncate()
+        json.dump(data, self.outfile)
+    
+    def close_spider(self, spider):
+        self.outfile.seek(0)
+        try:
+            data = json.load(self.outfile)
+        except json.JSONDecodeError:
+            spider.crawler.stop()
+        data['crawl-status'] = 'complete'
+        data['last-crawled'] = datetime.utcnow().isoformat()
+
+        self.outfile.seek(0)
+        self.outfile.truncate()
+        try:
+            json.dump(data, self.outfile)
+        except json.JSONDecodeError:
+            spider.crawler.stop()
+        self.outfile.close()
--- crawler/one_site_crawler/spiders/one_site_crawler.py
+++ crawler/one_site_crawler/spiders/one_site_crawler.py
@@ -1,8 +1,10 @@
-import scrapy
 from datetime import datetime
+from scrapy import Spider
+from scrapy import signals
+from urllib.parse import urlparse
 
 
-class OSCSpider(scrapy.Spider):
+class OSCSpider(Spider):
     name = "osc"
 
     def __init__(self, start_urls=None, allowed_domains=None, *args, **kwargs):
@@ -10,15 +12,19 @@ class OSCSpider(scrapy.Spider):
             self.start_urls = start_urls.split(',')
         
         if allowed_domains:
-            self.allowed_domains = allowed_domains.split(',')
+            domain_list = allowed_domains.split(',')
+            parsed_url = urlparse(domain_list[0])
+            if parsed_url.hostname:
+                self.allowed_domains = [parsed_url.hostname]
+            else:
+                self.allowed_domains = [domain_list[0]]
         
         super(OSCSpider, self).__init__(*args, **kwargs)
-
 
     def parse(self, response):
         yield {
             'url': response.url,
-            'timestamp': datetime.utcnow(),
+            'last-accessed': datetime.utcnow().isoformat(),
         }
         for link in response.css('a'):
             try:
